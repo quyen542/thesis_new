bf18dc152c49e08dc00c210984109c61970f6a6f
Quyen Duong <63244142+quyen542@users.noreply.github.com>
2024-05-03 13:29:05 +0700

    Update MvcConfig.java

--- src/main/java/com/example/thesis_new/config/MvcConfig.java
+++ src/main/java/com/example/thesis_new/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.example.thesis_new.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -8,6 +9,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Configuration
+@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
 
 
